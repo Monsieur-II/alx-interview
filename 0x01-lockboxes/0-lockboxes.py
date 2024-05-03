4804e48af84d03477dd756cbbf865c77f1942bfa
Evans Nyarko <evans.nyarko39@yahoo.com>
2024-05-03 05:10:51 +0000

    locked boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Locked boxes"""
+
+def canUnlockAll(boxes):
+    """Method that determines if all the boxes can be opened"""
+    keys = [0]
+    for key in keys:
+        for new_key in boxes[key]:
+            if new_key not in keys and new_key < len(boxes):
+                keys.append(new_key)
+    if len(keys) == len(boxes):
+        return True
+    return False
